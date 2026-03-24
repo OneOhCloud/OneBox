500f3f1883c39170e34d6c0fa20371d8f4bd2006
OneOh Cloud <github@oneoh.cloud>
2026-03-24 14:55:38 +0800

    refactor: simplify log cleanup logic and remove unused import in Windows VPN module

--- src-tauri/src/vpn/windows.rs
+++ src-tauri/src/vpn/windows.rs
@@ -10,7 +10,6 @@ use std::os::windows::ffi::OsStrExt;
 use windows::core::PCWSTR;
 use windows::Win32::UI::Shell::ShellExecuteW;
 
-use crate::vpn::helper;
 use crate::vpn::VpnProxy;
 // 默认绕过列表
 pub static DEFAULT_BYPASS: &str = "localhost;127.*;192.168.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;<local>";
