8b0cc55275774962f5676d416df961df2fcc69c2
OneOh Cloud <github@oneoh.cloud>
2026-03-28 03:06:42 +0800

    fix: update tar dependency versions and enhance update check with User-Agent header

--- src/utils/update.ts
+++ src/utils/update.ts
@@ -1,6 +1,7 @@
 import { check } from "@tauri-apps/plugin-updater";
 import { getStoreValue } from "../single/store";
 import { STAGE_VERSION_STORE_KEY } from "../types/definition";
+import { getSingBoxUserAgent } from "./helper";
 
 export const checkUpdate = async () => {
 
@@ -9,12 +10,14 @@ export const checkUpdate = async () => {
     if (stage === "stable") {
         stage = "latest"; // 稳定版直接使用最新版本
     }
+    const ua = await getSingBoxUserAgent()
 
     return await check({
         timeout: 5000, // 设置超时时间为5秒
         headers: {
             'Accept': 'application/json',
             'stage': stage,
+            'User-Agent': ua
         }
     });
 
